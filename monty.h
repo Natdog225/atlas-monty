0ee5b84acdaa5753a61f3b921b66548dc90dfb2c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-05 17:48:39 -0500

    Added prototype for pint

--- monty.h
+++ monty.h
@@ -38,6 +38,7 @@ typedef struct instruction_s
 /* Function prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 	/*(Prototypes for other opcodes: pint, pop, swap, add, sub, etc.) */
 	void
