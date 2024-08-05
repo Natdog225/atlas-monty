374160f04764cb3ed1caa0c6c9fee508c11f4244
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-05 17:01:46 -0500

    .h structure

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STACK_TYPE int
+#define EXIT_FAILURE 1
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -15,4 +21,28 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function prototypes */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+ 
+
+	/*(Prototypes for other opcodes: pint, pop, swap, add, sub, etc.) */
+	void
+	free_stack(stack_t *stack);
+int main(int argc, char *argv[]);
+
 #endif
