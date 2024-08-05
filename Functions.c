1f9a9b0447a2cdb7bda0a6edaf4c1940d17f27a7
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-05 17:43:59 -0500

    Added pint function.

--- Functions.c
+++ Functions.c
@@ -48,7 +48,22 @@ void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
 		current = current->next;
 	}
 }
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number of the  
+ instruction.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
+	printf("%d\n", (*stack)->n);
+}
 /**
  * is_digit - Checks if a string is a valid digit.
  * @str: The string to check.
